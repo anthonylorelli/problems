73c4a52422a4e5c5df0d5f5106333cdd3f2d7762
Anthony Lorelli <anthony.lorelli@outlook.com>
2018-10-07 19:02:25 -0700

    Set up problem

---
+++ uva10077/10077.cpp
@@ -0,0 +1,23 @@
+// Problem definition:
+// https://uva.onlinejudge.org/external/100/10077.pdf
+
+#include <iostream>
+
+void TraversePath(double numerator, double denominator)
+{
+    
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+
+    int numerator {0};
+    int denominator {0};
+
+    while (std::cin >> numerator && std::cin >> denominator && 
+        numerator != 1 && denominator != 1)
+    {
+        
+    }
+}
