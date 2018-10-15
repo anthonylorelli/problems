cfeee70cc0c005946d5b6f78d03fded9f867f353
Anthony Lorelli <anthony.lorelli@outlook.com>
2018-10-14 22:14:57 -0700

    Add initial (wrong) solution

--- uva10077/10077.cpp
+++ uva10077/10077.cpp
@@ -2,6 +2,46 @@
 // https://uva.onlinejudge.org/external/100/10077.pdf
 
 #include <iostream>
+#include <tuple>
+
+typedef std::pair<int, int> fraction;
+
+class SternBrocot
+{
+public:
+    SternBrocot(const int targetNumerator, const int targetDenominator) :
+        m_targetNumerator {targetNumerator}, m_targetDenominator {targetDenominator} {}
+
+    void PrintPath() { PrintPath(m_left, m_identity, m_right); }
+
+private:
+    void PrintPath(const fraction& left, const fraction& current, const fraction& right)
+    {
+        if (current.first == m_targetNumerator && current.second == m_targetDenominator) 
+        { 
+            std::cout << "\n";
+            return; 
+        }
+
+        if (m_targetNumerator * current.second > current.first * m_targetDenominator)
+        {
+            std::cout << "R";
+            PrintPath(current, std::make_pair(current.first + right.first, current.second + right.second), right);
+        }
+        else
+        {
+            std::cout << "L";
+            PrintPath(left, std::make_pair(current.first + left.first, current.second + left.second), current);
+        }
+    }
+
+    const int m_targetNumerator;
+    const int m_targetDenominator;
+
+    const fraction m_left = std::make_pair(0, 1);
+    const fraction m_identity = std::make_pair(1, 1);
+    const fraction m_right = std::make_pair(1, 0);
+};
 
 int main()
 {
@@ -13,6 +53,7 @@ int main()
     while (std::cin >> numerator && std::cin >> denominator && 
         numerator != 1 && denominator != 1)
     {
-        
+        SternBrocot sb {numerator, denominator};
+        sb.PrintPath();
     }
 }
