b91fca68559ba4f5d53ec0728572ca7f840781fc
Anthony Lorelli <anthony.lorelli@outlook.com>
2018-10-22 21:31:49 -0700

    Add initial problem definitions for 10742 and 10706

---
+++ uva10706/10706.cpp
@@ -0,0 +1,19 @@
+// Problem definition:
+// https://uva.onlinejudge.org/external/107/10706.pdf
+// Accepted ?
+
+#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+
+    int cases {0};
+    std::cin >> cases;
+
+    while (cases--)
+    {
+        int target {0};
+        std::cin >> target;
+    }
+}
