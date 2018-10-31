ed13574401995a792f57557eee9ff5612afc6d34
Anthony Lorelli <anthony.lorelli@outlook.com>
2018-10-30 22:07:07 -0700

    Add sieve reference

--- uva/10742/10742.cpp
+++ uva/10742/10742.cpp
@@ -2,17 +2,23 @@
 // Accepted ?
 
 #include <iostream>
+#include <vector>
+
+void FillPrimes(std::vector<int> numbers)
+{
+    // https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+}
 
 int main()
 {
     std::ios_base::sync_with_stdio(false);
 
-    int cases {0};
-    std::cin >> cases;
-
-    while (cases--)
+    int price {0};
+    int count {1};
+    
+    while (std::cin >> price && price)
     {
-        int target {0};
-        std::cin >> target;
+        int combinations {0};
+        std::cout << "Case #" << count << ": " << combinations << "\n";
     }
 }
