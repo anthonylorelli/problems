902517b8ac7eb88996baa831e573e80ba4c2d113
Anthony Lorelli <anthony.lorelli@outlook.com>
2018-11-01 20:53:32 -0700

    Add initial sieve implementation

--- uva/10742/10742.cpp
+++ uva/10742/10742.cpp
@@ -4,14 +4,36 @@
 #include <iostream>
 #include <vector>
 
-void FillPrimes(std::vector<int> numbers)
+void GeneratePrimes(std::vector<int>& numbers)
 {
     // https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    constexpr int max {100};
+    std::vector<int> v;
+
+    for (int i = 0; i <= max; ++i) { v.push_back(i); }
+
+    for (int i = 2; i <= max; ++i)
+    {
+        if (v[i])
+        {
+            for (int j = 1; i + (j * i) < max; ++j)
+            {
+                v[i + (j * i)] = 0;
+            }
+        }
+    }
+
+    for (auto i : v) { if (i) { numbers.push_back(i); } }
 }
 
 int main()
 {
     std::ios_base::sync_with_stdio(false);
+
+    std::vector<int> numbers;
+    GeneratePrimes(numbers);
+
+    for (auto i : numbers) { std::cout << i << "\n"; }
 
     int price {0};
     int count {1};
@@ -19,6 +41,7 @@ int main()
     while (std::cin >> price && price)
     {
         int combinations {0};
-        std::cout << "Case #" << count << ": " << combinations << "\n";
+
+        std::cout << "Case #" << count++ << ": " << combinations << "\n";
     }
 }
