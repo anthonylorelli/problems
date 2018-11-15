8e02d00e76cb443f26ae84d1eea333e918e775d3
Anthony Lorelli <anthony.lorelli@outlook.com>
2018-11-14 21:29:57 -0800

    Add setup files for 11701 and 11876

---
+++ uva/11701/11701.cpp
@@ -0,0 +1,11 @@
+// Problem definition: https://uva.onlinejudge.org/external/117/11701.pdf
+// Accepted ?
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+
+}
