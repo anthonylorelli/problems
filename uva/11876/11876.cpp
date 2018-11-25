ef326913586be473ca2aed40d36534f171178d61
Anthony Lorelli <anthony.lorelli@outlook.com>
2018-11-24 21:13:40 -0800

    Add scaffolding for 11876

--- uva/11876/11876.cpp
+++ uva/11876/11876.cpp
@@ -7,5 +7,14 @@
 int main()
 {
     std::ios_base::sync_with_stdio(false);
+    int cases {0};
+    std::cin >> cases;
 
+    while (--cases)
+    {
+        int rangeStart {0};
+        int rangeEnd {0};
+        std::cin >> rangeStart >> rangeEnd;
+        std::cout << rangeStart << " " << rangeEnd << "\n";
+    }
 }
