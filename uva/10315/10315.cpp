28ee708a6986a08e8eac068c989baed614fb52cb
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-04-01 21:57:10 -0700

    Use type alias for pair representing cards

--- uva/10315/10315.cpp
+++ uva/10315/10315.cpp
@@ -8,6 +8,7 @@
 #include <unordered_map>
 
 constexpr size_t handSize{5};
+using card = std::pair<int,char>;
 
 std::unordered_map<char,int> rankToValue{
     {'2',2}, {'3',3}, {'4',4},{'5',5}, {'6',6}, {'7',7}, {'8',8}, 
@@ -19,10 +20,10 @@ std::unordered_map<int,char> valueToRank{
     {9,'9'}, {10,'T'}, {11,'J'}, {12,'Q'}, {13,'K'}, {14,'A'}
 };
 
-std::ostream& operator<<(std::ostream& o, std::array<std::pair<int,char>,handSize> hand)
+std::ostream& operator<<(std::ostream& o, std::array<card,handSize> hand)
 {
     o << "Hand: ";
-    std::for_each(hand.begin(), hand.end(), [&o](std::pair<int,char>& c) { o << " " << valueToRank[c.first] << c.second; });
+    std::for_each(hand.begin(), hand.end(), [&o](card& c) { o << " " << valueToRank[c.first] << c.second; });
     o << "\n";
     return o;
 }
@@ -40,7 +41,7 @@ enum class HandType
     StraightFlush
 };
 
-bool IsNOfAKind(const std::array<std::pair<int,char>,handSize>& hand, const int n)
+bool IsNOfAKind(const std::array<card,handSize>& hand, const int n)
 {
     int count{1};
     int rank{hand[0].first};
@@ -53,64 +54,64 @@ bool IsNOfAKind(const std::array<std::pair<int,char>,handSize>& hand, const int 
     return count == n;
 }
 
-bool IsTwoPairs(const std::array<std::pair<int,char>,handSize>& hand)
+bool IsTwoPairs(const std::array<card,handSize>& hand)
 {
-    auto findPair{[](const std::pair<int,char>& c1, const std::pair<int,char>& c2) { return c1.first == c2.first; }};
+    auto findPair{[](const card& c1, const card& c2) { return c1.first == c2.first; }};
     auto first{std::adjacent_find(hand.begin(), hand.end(), findPair)};
     auto second{(first != hand.end() && first + 2 != hand.end()) ?
         std::adjacent_find(first+2, hand.end(), findPair) : hand.end()};
     return second != hand.end();
 }
 
-bool IsStraight(const std::array<std::pair<int,char>,handSize>& hand)
+bool IsStraight(const std::array<card,handSize>& hand)
 {
     int rank{hand[0].first + 1};
     return std::all_of(hand.begin()+1, hand.end(), 
-        [&rank](const std::pair<int,char> c) { return c.first == rank++; });
+        [&rank](const card& c) { return c.first == rank++; });
 }
 
-bool IsStraightFlush(const std::array<std::pair<int,char>,handSize>& hand)
+bool IsStraightFlush(const std::array<card,handSize>& hand)
 {
     int rank{hand[0].first + 1};
     char suit{hand[0].second};
     return std::all_of(hand.begin()+1, hand.end(),
-        [&rank, &suit](const std::pair<int,char> c) { return c.first == rank++ && c.second == suit; });
+        [&rank, &suit](const card& c) { return c.first == rank++ && c.second == suit; });
 }
 
-std::pair<int,char> HighCard(const std::array<std::pair<int,char>,handSize>& hand)
+card HighCard(const std::array<card,handSize>& hand)
 {
     return std::make_pair<int,char>('\0','\0');
 }
 
-bool IsPair(const std::array<std::pair<int,char>,handSize>& hand)
+bool IsPair(const std::array<card,handSize>& hand)
 {
     return IsNOfAKind(hand, 2);
 }
 
-bool IsThreeOfAKind(const std::array<std::pair<int,char>,handSize>& hand)
+bool IsThreeOfAKind(const std::array<card,handSize>& hand)
 {
     return IsNOfAKind(hand, 3);
 }
 
-bool IsFullHouse(const std::array<std::pair<int,char>,handSize>& hand)
+bool IsFullHouse(const std::array<card,handSize>& hand)
 {
     return (hand[0].first == hand[1].first && hand[1].first == hand[2].first && hand[3].first == hand[4].first) ||
         (hand[0].first == hand[1].first && hand[2].first == hand[3].first && hand[3].first == hand[4].first);
 }
 
-bool IsFourOfAKind(const std::array<std::pair<int,char>,handSize>& hand)
+bool IsFourOfAKind(const std::array<card,handSize>& hand)
 {
     return IsNOfAKind(hand, 4);
 }
 
-bool IsFlush(const std::array<std::pair<int,char>,handSize>& hand)
+bool IsFlush(const std::array<card,handSize>& hand)
 {
     const char suit{hand[0].second};
     return std::all_of(hand.begin()+1, hand.end(), 
-        [&suit](const std::pair<int,char>& c) { return c.second == suit; });
+        [&suit](const card& c) { return c.second == suit; });
 }
 
-HandType ClassifyHand(const std::array<std::pair<int,char>,handSize>& hand)
+HandType ClassifyHand(const std::array<card,handSize>& hand)
 {
     return HandType::HighCard;
 }
@@ -119,16 +120,16 @@ int main()
 {
     std::ios_base::sync_with_stdio(false);
 
-    std::array<std::pair<int,char>,handSize> blackHand;
-    std::array<std::pair<int,char>,handSize> whiteHand;
+    std::array<card,handSize> blackHand;
+    std::array<card,handSize> whiteHand;
 
     char rank, suit;
-    auto assignCard{[&rank](std::pair<int,char>& c) 
+    auto assignCard{[&rank](card& c) 
     { 
         std::cin >> rank >> c.second; 
         c.first = rankToValue[rank];
     }};
-    auto sortHand([](const std::pair<int,char>& c1, const std::pair<int,char>& c2) { return c1.first < c2.first; });
+    auto sortHand([](const card& c1, const card& c2) { return c1.first < c2.first; });
 
     while (std::cin >> rank >> suit)
     {
