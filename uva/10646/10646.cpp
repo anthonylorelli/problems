aa68522e63452c4fb531de591cdb40832ccffd3e
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-06-05 22:01:13 -0700

    Remove commented lines

--- uva/10646/10646.cpp
+++ uva/10646/10646.cpp
@@ -16,8 +16,6 @@ class Deck
 {
 public:
     void fill(std::istream& i) {
-        //std::generate_n(m_deck.rbegin(), deckSize,
-         //   [&i](){ card c; i >> c.first; i >> c.second; return c; });
         std::for_each(m_deck.rbegin(), m_deck.rend(),
             [&i](card& c){ i >> c.first; i >> c.second; });
     }
