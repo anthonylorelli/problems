11006de4436e2df5af15ec69498e5368b0a8ace4
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-06-13 21:37:07 -0700

    Add basic set support and test cases for hand building

--- uva/11678/11678.cpp
+++ uva/11678/11678.cpp
@@ -5,7 +5,27 @@
 #define CATCH_CONFIG_RUNNER
 #include "../catch/catch.hpp"
 
+#include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <set>
+
+class Trade
+{
+public:
+    Trade(int alice, int betty, std::istream& i) {
+        auto extract{[&i]() -> int { int c; i >> c; return c; }};
+        std::generate_n(std::inserter(m_alice, m_alice.end()), alice, extract);
+        std::generate_n(std::inserter(m_betty, m_betty.end()), betty, extract);
+    }
+
+    const size_t alice() const { return m_alice.size(); }
+    const size_t betty() const { return m_betty.size(); }
+
+private:
+    std::set<int> m_alice;
+    std::set<int> m_betty;
+};
 
 int execute(std::istream& in, std::ostream& out) {
 
@@ -15,12 +35,28 @@ int execute(std::istream& in, std::ostream& out) {
 int main(int argc, char* argv[]) {
     std::ios_base::sync_with_stdio(false);
 
-    //return Catch::Session().run(argc, argv);
-    return execute(std::cin, std::cout);
+    return Catch::Session().run(argc, argv);
+    //return execute(std::cin, std::cout);
 }
 
-TEST_CASE("", "[Exchanging cards]") {
-    SECTION("") {
+TEST_CASE("Size", "[Exchanging cards]") {
+    SECTION("No repeats") {
+        std::istringstream i{"1\n1 2 3 4 5 10 100 1000 10000 100000\n"};
+        Trade t{1, 10, i};
+        REQUIRE(t.alice() == 1);
+        REQUIRE(t.betty() == 10);
+    }
+    SECTION("Alice repeats") {
+        std::istringstream i{"1 1 1 1 1 1 2 3\n1 2 3 4 5 10 100 1000 10000 100000\n"};
+        Trade t{8, 10, i};
+        REQUIRE(t.alice() == 3);
+        REQUIRE(t.betty() == 10);
+    }
+    SECTION("Betty repeats") {
+        std::istringstream i{"1 2 3\n1 1 2 2 3 3 4 4 5 5 10 10 100 100 1000 1000 10000 10000 100000 100000\n"};
+        Trade t{3, 20, i};
+        REQUIRE(t.alice() == 3);
+        REQUIRE(t.betty() == 10);
     }
 }
 
