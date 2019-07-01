4eefb3045ca0faeda806675db9a46604d90344b7
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-06-30 21:21:56 -0700

    Create scaffold for LC 771

---
+++ lc/0771/0771.cpp
@@ -0,0 +1,29 @@
+// 771. Jewels and Stones
+// Problem definition: https://leetcode.com/problems/jewels-and-stones/
+// Accepted ?
+
+#define CATCH_CONFIG_RUNNER
+#include "../../uva/catch/catch.hpp"
+
+#include <string>
+
+class Solution {
+public:
+    int numJewelsInStones(std::string J, std::string S) {
+        
+    }
+};
+
+int execute(std::istream& in, std::ostream& out) {
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+
+    return Catch::Session().run(argc, argv);
+    //return execute(std::cin, std::cout);
+}
+
+TEST_CASE("", "[Jewels and Stones]") {
+}
