f98e31f7a3770da77900273ed4a22592b42c578d
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-08-04 20:22:33 -0700

    Handle more of the corner cases, but still not all

--- lc/0523/0523.cpp
+++ lc/0523/0523.cpp
@@ -6,19 +6,24 @@
 #include "../../uva/catch/catch.hpp"
 
 #include <vector>
+#include <unordered_map>
 
 class Solution {
 public:
     bool checkSubarraySum(std::vector<int>& nums, int k) {
         if (nums.size() < 2) { return false; }
-        std::vector<int> v(k);
+        std::unordered_map<int,size_t> m;
         int sum {0};
-        v[0] = 1;
-        for (const auto n : nums) {
-            sum += n;
-            int remainder {sum % k};
-            if (v[remainder]) { return true; }
-            v[remainder]++;
+        const int ak {k < 0 ? -k : k};
+        m[0] = 0;
+        for (size_t i {0}; i < nums.size(); ++i) {
+            sum += nums[i];
+            if (i > 0 && nums[i] == 0 && nums[i-1] == 0) { return true; }
+            if (ak) {
+                int remainder {sum % ak};
+                if (m.count(remainder) && (i - m[remainder] > 1)) { return true; }
+                if (!m.count(remainder)) { m[remainder] = i; }
+            }
         }
         return false;
     }
@@ -54,5 +59,35 @@ TEST_CASE("", "[Continuous Subarray Sum]") {
         Solution s;
         std::vector<int> v = { 6 };
         REQUIRE_FALSE(s.checkSubarraySum(v, 6));
+    }
+    SECTION("Local test case 4") {
+        Solution s;
+        std::vector<int> v = { 1, 6 };
+        REQUIRE_FALSE(s.checkSubarraySum(v, 6));
+    }
+    SECTION("Local test case 5") {
+        Solution s;
+        std::vector<int> v = { 1, 4, 2 };
+        REQUIRE(s.checkSubarraySum(v, 6));
+    }
+    SECTION("Local test case 6") {
+        Solution s;
+        std::vector<int> v = { 1, 4, 2 };
+        REQUIRE_FALSE(s.checkSubarraySum(v, 0));
+    }
+    SECTION("Local test case 7") {
+        Solution s;
+        std::vector<int> v = { 0, 0 };
+        REQUIRE(s.checkSubarraySum(v, 0));
+    }
+    SECTION("Local test case 8") {
+        Solution s;
+        std::vector<int> v = { 1, 0, 0 };
+        REQUIRE(s.checkSubarraySum(v, 0));
+    }
+    SECTION("Local test case 9") {
+        Solution s;
+        std::vector<int> v = { 1, 1 };
+        REQUIRE(s.checkSubarraySum(v, -1));
     }
 }
