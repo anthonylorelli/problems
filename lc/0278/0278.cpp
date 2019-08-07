80750efce63eed8412c042772accfcbbbc6f1347
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-08-06 21:01:32 -0700

    Simplify solution for accepted result

--- lc/0278/0278.cpp
+++ lc/0278/0278.cpp
@@ -1,28 +1,27 @@
 // 0278. First Bad Version
 // Problem definition: https://leetcode.com/problems/first-bad-version/
-// Accepted ?
+// Accepted 2019-08-06
 
 #define CATCH_CONFIG_RUNNER
 #include "../../uva/catch/catch.hpp"
-
-#include <vector>
-#include <algorithm>
-#include <numeric>
 
 // Forward declaration of isBadVersion API.
 bool isBadVersion(int version);
 
 class Solution {
 public:
+    int firstBadVersion(int n, int max) {
+        const bool result {isBadVersion(n)};
+        if (result) {
+            return firstBadVersion(n / 2, n);
+        } else if (n + 1 == max) {
+            return max;
+        } else {
+            return firstBadVersion(n + ((max - n) / 2), max);
+        }
+    }
     int firstBadVersion(int n) {
-        std::vector<int> v(n);
-        std::iota(v.begin(), v.end(), 1);
-        auto first {std::upper_bound(v.begin(), v.end(), 1, [](const int i, const int j) { 
-            const bool iIsBad {isBadVersion(i)};
-            const bool jIsBad {isBadVersion(j)};
-            return !iIsBad && jIsBad ? true : false;
-        })};
-        return first != v.end() ? *first : *v.begin();
+        return firstBadVersion(n / 2, n);
     }
 };
 
