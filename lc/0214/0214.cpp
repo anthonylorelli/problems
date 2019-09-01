e0eb98ac14697891fdea65577dd9077c5c59eb28
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-08-31 18:12:02 -0700

    Add basic suffix tree implementation

--- lc/0214/0214.cpp
+++ lc/0214/0214.cpp
@@ -8,11 +8,61 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
+
+class Trie {
+public:
+    void insert(std::string::iterator begin, std::string::iterator end) {
+        Trie* current {nullptr};
+        while (begin != end) {
+            current = &m_children[*begin];
+            begin++;
+        }
+    }
+
+    template <typename T>
+    int match(T b, T e) {
+        Trie* current {this};
+        int count {0};
+        while (b++ != e) {
+            if (current->m_children.count(*b)) {
+                current = &current->m_children[*b];
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+private:
+    std::unordered_map<char,Trie> m_children;
+};
+
+class SuffixTree {
+public:
+    void insert(std::string& s) {
+        auto b {s.begin()};
+        auto e {s.end()};
+        while (b != e) { m_trie.insert(b++, e); }
+    }
+
+    template <typename T>
+    int match(T b, T e) {
+        return m_trie.match(b, e);
+    }
+
+private:
+    Trie m_trie;
+};
 
 class Solution {
 public:
     std::string shortestPalindrome(std::string s) {
-        // Suffix trees?
+        SuffixTree t;
+        t.insert(s);
+        int longestMatch {t.match(s.rbegin(), s.rend())};
+        return "";
     }
 };
 
