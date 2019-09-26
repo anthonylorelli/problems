9522824d8aca929f3c698fc0280325b0b5bd548c
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-09-25 21:32:41 -0700

    Create scaffold for 0005

---
+++ lc/0005/0005.cpp
@@ -0,0 +1,35 @@
+// 0005. Longest Palindromic Substring
+// Problem definition: https://leetcode.com/problems/longest-palindromic-substring/
+// Accepted ?
+
+#define CATCH_CONFIG_RUNNER
+#include "../../uva/catch/catch.hpp"
+
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    std::string longestPalindrome(std::string s) {
+        return "";
+    }
+};
+
+TEST_CASE("LC test cases", "[Longest Palindromic Substring]") {
+    Solution s;
+    std::vector<std::pair<std::string,std::string>> input {
+        {"",""}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&s, &input](auto& p) { 
+                REQUIRE(s.longestPalindrome(p.first) == p.second); 
+            });
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
