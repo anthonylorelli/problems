9efd714848b9f793fcd76c157825e7ec99195d98
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-09-25 21:26:15 -0700

    Add Xor-style solution and acceptance date

--- lc/0268/0268.cpp
+++ lc/0268/0268.cpp
@@ -1,6 +1,6 @@
 // 0268. Missing Number
 // Problem definition: https://leetcode.com/problems/missing-number/
-// Accepted ?
+// Accepted 2019-09-25
 
 #define CATCH_CONFIG_RUNNER
 #include "../../uva/catch/catch.hpp"
@@ -11,6 +11,14 @@
 class Solution {
 public:
     int missingNumber(std::vector<int>& nums) {
+        size_t missing {nums.size()};
+        for (size_t i {0}; i < nums.size(); ++i) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+
+    int missingNumberIndexAlignment(std::vector<int>& nums) {
         for (size_t i {0}; i < nums.size(); ++i) {
             auto current {nums[i]};
             if (current < nums.size() && i != current) {
@@ -30,7 +38,8 @@ public:
 TEST_CASE("LC test cases", "[Missing Number]") {
     Solution s;
     std::vector<std::pair<std::vector<int>,int>> input {
-        {{},0}, {{3, 0, 1}, 2}, {{9,6,4,2,3,5,7,0,1}, 8}
+        {{},0}, {{3, 0, 1}, 2}, {{9,6,4,2,3,5,7,0,1}, 8}, {{1}, 0},
+        {{0}, 1}
     };
 
     SECTION("LC test cases") {
