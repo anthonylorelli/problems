cc34ee833b2340d341d4e75fb38c55512a95a143
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-09-25 21:07:25 -0700

    Add first take on finding missing number

--- lc/0268/0268.cpp
+++ lc/0268/0268.cpp
@@ -6,18 +6,31 @@
 #include "../../uva/catch/catch.hpp"
 
 #include <vector>
+#include <algorithm>
 
 class Solution {
 public:
     int missingNumber(std::vector<int>& nums) {
-        return 0;
+        for (size_t i {0}; i < nums.size(); ++i) {
+            auto current {nums[i]};
+            if (current < nums.size() && i != current) {
+                std::swap(nums[i], nums[current]);
+                if (nums[i] != nums[current]) { --i; }
+            }
+        }
+
+        for (size_t i {0}; i < nums.size(); ++i) {
+            if (nums[i] != i) { return i; }
+        }
+
+        return nums.size();
     }
 };
 
-TEST_CASE("LC test cases", "[Shortest Palindrome]") {
+TEST_CASE("LC test cases", "[Missing Number]") {
     Solution s;
     std::vector<std::pair<std::vector<int>,int>> input {
-        {{},0}
+        {{},0}, {{3, 0, 1}, 2}, {{9,6,4,2,3,5,7,0,1}, 8}
     };
 
     SECTION("LC test cases") {
