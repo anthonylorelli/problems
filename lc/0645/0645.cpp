558b8be15c74d4b7a4929f24918724dbb537bf4f
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-10-09 20:34:45 -0700

    Finish first iteration on solution and add test case

--- lc/0645/0645.cpp
+++ lc/0645/0645.cpp
@@ -6,24 +6,43 @@
 #include "../../uva/catch/catch.hpp"
 
 #include <vector>
+#include <algorithm>
 
 class Solution {
 public:
     std::vector<int> findErrorNums(std::vector<int>& nums) {
-        return {};
+        std::vector<int> result;
+
+        for (int i {0}; i < nums.size(); ++i) {
+            int current {nums[i]}, target {i+1};
+            if (current != target) {
+                std::swap(nums[i], nums[target-1]);
+                if (nums[current - 1] != nums[i]) { --i; }
+            }
+        }
+
+        for (int i {0}; i < nums.size(); ++i) {
+            if (nums[i] != i+1) {
+                result.push_back(nums[i]);
+                result.push_back(i + 1);
+                return result;
+            }
+        }
+
+        return result;
     }
 };
 
 TEST_CASE("LC test cases", "[Set Mismatch]") {
     Solution s;
     std::vector<std::pair<std::vector<int>,std::vector<int>>> input {
-        {{1,2,2,4},{2,3}}
+        {{1,2,2,4},{2,3}}, {{3,2,2},{2,1}}
     };
 
     SECTION("LC test cases") {
         std::for_each(std::begin(input), std::end(input),
             [&s, &input](auto& p) { 
-                REQUIRE(s.swimInWater(p.first) == p.second); 
+                REQUIRE(s.findErrorNums(p.first) == p.second); 
             });
     }
 }
