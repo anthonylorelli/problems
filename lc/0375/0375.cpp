6ad05c9d99ac08146bba9e0f9535e599cf7db8bf
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-11-12 21:34:50 -0800

    Create scaffold for 0375

---
+++ lc/0375/0375.cpp
@@ -0,0 +1,34 @@
+// 0375. Guess Number Higher or Lower II
+// Problem definition: https://leetcode.com/problems/guess-number-higher-or-lower-ii/
+// Accepted ?
+
+#define CATCH_CONFIG_RUNNER
+#include "../../uva/catch/catch.hpp"
+
+#include <vector>
+
+class Solution {
+public:
+    int getMoneyAmount(int n) {
+        return 0;
+    }
+};
+
+TEST_CASE("LC test cases", "[Guess Number Higher or Lower II]") {
+    Solution s;
+    std::vector<std::pair<int,int>> input {
+        {{10,21},{1,0}}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&s, &input](auto& p) { 
+                REQUIRE(s.getMoneyAmount(p.first) == p.second); 
+            });
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
