060a26c18da95bedc24b98b68eea1632e0293c48
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-11-19 19:17:58 -0800

    Fix argument order bug

--- lc/0375/0375.cpp
+++ lc/0375/0375.cpp
@@ -12,38 +12,38 @@
 class Solution {
 public:
     template <typename T>
-    int getMoneyAmount(T& t, int left, int right) {
-        if (t[left][right]) {
-            return t[left][right];
+    int getMoneyAmount(T& t, int row, int col) {
+        if (t[row][col]) {
+            return t[row][col];
         }
 
-        if (left >= right) {
+        if (row >= col) {
             return 0;
-        } else if (left >= (right - 2)) {
-            return right - 1;
+        } else if (row >= (col - 2)) {
+            return col - 1;
         }
 
-        int mid {left + (right - left) / 2}, min {std::numeric_limits<int>::max()};
+        int mid {row + (col - row) / 2}, min {std::numeric_limits<int>::max()};
 
-        for (int i {mid}; i < right; ++i) {
-            min = std::min(min, i + std::max(getMoneyAmount(t, i + 1, right), getMoneyAmount(t, left, i - 1)));
+        for (int i {mid}; i < col; ++i) {
+            min = std::min(min, i + std::max(getMoneyAmount(t, i + 1, col), getMoneyAmount(t, row, i - 1)));
         }
 
-        t[left][right] = min;
+        t[row][col] = min;
 
         return min;
     }
 
     int getMoneyAmount(int n) {
         std::vector<std::vector<int>> table{n+1u, std::vector<int>(n+1u, 0)};
-        return getMoneyAmount(table, n, 1);
+        return getMoneyAmount(table, 1, n);
     }
 };
 
 TEST_CASE("LC test cases", "[Guess Number Higher or Lower II]") {
     Solution s;
     std::vector<std::pair<int,int>> input {
-        {{10,21},{1,0},{3,2}}
+        {{10,16},{1,0},{3,2}}
     };
 
     SECTION("LC test cases") {
