8b93a11ea62b707918ab7547c1566411586f5e0d
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-12-10 22:09:14 -0800

    Complete initial implementation

--- lc/1109/1109.cpp
+++ lc/1109/1109.cpp
@@ -10,13 +10,22 @@
 class Solution {
 public:
     std::vector<int> corpFlightBookings(std::vector<std::vector<int>>& bookings, int n) {
-        return {};
+        std::vector<int> seats(n, 0);
+        for (const auto& flights : bookings) {
+            int start {flights[0]-1};
+            int end {flights[1]-1};
+            int booked {flights[2]};
+            for (int i {start}; i <= end; ++i) {
+                seats[i] += booked;
+            }
+        }
+        return seats;
     }
 };
 
 TEST_CASE("LC test cases", "[Corporate Flight Bookings]") {
     std::vector<std::pair<std::pair<std::vector<std::vector<int>>,int>,std::vector<int>>> input {
-        {{{{1,2,10},{2,3,20},{2,5,25}},5},{10,55,45,25,55}}
+        {{{{1,2,10},{2,3,20},{2,5,25}},5},{10,55,45,25,25}}
     };
 
     SECTION("LC test cases") {
