a162f1a8433c47bdb8417cc1388730b12daa0a10
Anthony Lorelli <anthony.lorelli@outlook.com>
2019-12-10 22:15:23 -0800

    Add more test cases

--- lc/1109/1109.cpp
+++ lc/1109/1109.cpp
@@ -25,7 +25,9 @@ public:
 
 TEST_CASE("LC test cases", "[Corporate Flight Bookings]") {
     std::vector<std::pair<std::pair<std::vector<std::vector<int>>,int>,std::vector<int>>> input {
-        {{{{1,2,10},{2,3,20},{2,5,25}},5},{10,55,45,25,25}}
+        {{{{1,2,10},{2,3,20},{2,5,25}},5},{10,55,45,25,25}},
+        {{{{1,1,10},{1,1,20},{1,1,25}},1},{55}},
+        {{{{1,1,10},{3,3,20},{5,5,25}},5},{10,0,20,0,25}}
     };
 
     SECTION("LC test cases") {
