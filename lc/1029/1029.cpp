77e6524a4f8c499d58203753b323a0315ec8fc6d
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-01-01 16:34:25 -0800

    Convert to greatest difference-based approach for acceptable solution

--- lc/1029/1029.cpp
+++ lc/1029/1029.cpp
@@ -1,6 +1,6 @@
 // 1029. Two City Scheduling
 // Problem definition: https://leetcode.com/problems/two-city-scheduling/
-// Accepted ?
+// Accepted 2020-01-01
 
 #define CATCH_CONFIG_RUNNER
 #include "../../uva/catch/catch.hpp"
@@ -12,18 +12,14 @@
 class Solution {
 public:
     int twoCitySchedCost(std::vector<std::vector<int>>& costs) {
-        size_t n {costs.size() / 2};
         std::sort(costs.begin(), costs.end(), [](const auto& v1, const auto& v2) {
-            return v1[0] == v2[0] ? v2[0] < v2[0] : v1[0] < v2[0];
+            return v1[1] - v1[0] > v2[1] - v2[0];
         });
-        int total = std::accumulate(costs.begin(), costs.begin() + n, 0, [](int sum, auto& v) {
-            v[1] += 1000;
+        const auto n {costs.size() / 2};
+        int total = std::accumulate(costs.begin(), costs.begin() + n, 0, [](int sum, const auto& v) {
             return sum + v[0];
         });
-        std::sort(costs.begin(), costs.end(), [](const auto& v1, const auto& v2) {
-            return v1[1] < v2[1];
-        });
-        total = std::accumulate(costs.begin(), costs.begin() + n, total, [](int sum, const auto& v) {
+        total = std::accumulate(costs.begin() + n, costs.end(), total, [](int sum, const auto& v) {
             return sum + v[1];
         });
         return total;
@@ -32,7 +28,10 @@ public:
 
 TEST_CASE("LC test cases", "[Two City Scheduling]") {
     std::vector<std::pair<std::vector<std::vector<int>>,int>> input {
-        {{{10,20},{30,200},{400,50},{30,20}},110}
+        {{{10,20},{30,200},{400,50},{30,20}},110},
+        {{{20,10},{200,30},{50,400},{20,30}},110},
+        {{{999,1},{999,2},{999,3},{999,4}},2001},
+        {{{1,999},{2,999},{3,999},{4,999}},2001}
     };
 
     SECTION("LC test cases") {
