318bc9a323ae84a3062c44dcf79f8ce2173cb349
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-01-26 20:35:44 -0800

    Fix loop width and re-add test cases

--- lc/1092/1092.cpp
+++ lc/1092/1092.cpp
@@ -91,23 +91,19 @@ private:
     std::string& m_word2;
 
     void build(const size_t i, const size_t j, std::string& output) {
-        std::cout << "i " << i << " j " << j << "\n";
         if (m_m[i][j].op == Op::none) { return; }
 
         switch (m_m[i][j].op) {
         case Op::insert:
-            build(i - 1, j, output);
-            std::cout << m_word2[j-1] << "\n";
+            build(i, j - 1, output);
             output.push_back(m_word2[j-1]);
             break;
         case Op::remove:
-            build(i, j - 1, output);
-            std::cout << m_word1[i-1] << "\n";
+            build(i - 1, j, output);
             output.push_back(m_word1[i-1]);
             break;
         case Op::match:
             build(i - 1, j - 1, output);
-            std::cout << m_word1[i-1] << "\n";
             output.push_back(m_word1[i-1]);
             break;
         default:
@@ -146,7 +142,7 @@ private:
     Cell remove_cost(const char c) const { return Cell{1,Op::remove}; }
 
     Cell& goal(std::vector<std::vector<Cell>>& m, const std::string& w1, const std::string& w2) const {
-        return m[w1.length()-1][w2.length()-1];
+        return m[w1.length()][w2.length()];
     }
 
     void fill(std::string& word1, std::string& word2, std::vector<std::vector<Cell>>& m) const {
@@ -157,8 +153,8 @@ private:
 
         Cell comparison, insert, remove;
 
-        for (size_t i{1}; i < word1.length(); ++i) {
-            for (size_t j{1}; j < word2.length(); ++j) {
+        for (size_t i{1}; i <= word1.length(); ++i) {
+            for (size_t j{1}; j <= word2.length(); ++j) {
                 const char w1 {word1[i-1]}, w2 {word2[j-1]}; 
                 comparison = m[i-1][j-1].cost + match_cost(w1, w2);
                 insert = m[i][j-1].cost + insert_cost(w2);
@@ -170,6 +166,7 @@ private:
     }
 
 public:
+
     int distance(std::string& word1, std::string& word2) const {
         const size_t max{std::max(word1.length(), word2.length()) + 1};
         std::vector<std::vector<Cell>> m(max, std::vector<Cell>(max));
@@ -215,24 +212,24 @@ public:
 //     }
 // }
 
-// TEST_CASE("Edit distance supersequence", "[Edit Distance]") {
-//     std::vector<std::pair<std::pair<std::string,std::string>,std::string>> input {
-//         {{"abc","def"},"abcdef"}
-//     };
+TEST_CASE("Edit distance supersequence", "[Edit Distance]") {
+    std::vector<std::pair<std::pair<std::string,std::string>,std::string>> input {
+        {{"abc","def"},"abcdef"},{{"abcd","bcde"},"abcde"}
+    };
 
-//     SECTION("Edit distance test cases") {
-//         std::for_each(std::begin(input), std::end(input),
-//             [&input](auto& p) { 
-//                 Distance d;
-//                 auto& [testInput, expected] = p;
-//                 REQUIRE(d.supersequence(testInput.first, testInput.second) == expected);
-//             });
-//     }
-// }
+    SECTION("Edit distance test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) { 
+                Distance d;
+                auto& [testInput, expected] = p;
+                REQUIRE(d.supersequence(testInput.first, testInput.second) == expected);
+            });
+    }
+}
 
 TEST_CASE("Edit distance", "[Edit Distance]") {
     std::vector<std::pair<std::pair<std::string,std::string>,int>> input {
-        {{"abc","def"},6}
+        {{"abc","def"},6},{{"horse","ors"},2},{{"intention","execution"},8}
     };
 
     SECTION("Edit distance test cases") {
