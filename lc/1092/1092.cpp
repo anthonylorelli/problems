2c161604262c7d5e7af8f3be079cf93c6809be48
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-01-27 19:57:17 -0800

    Remove suffix tree

--- lc/1092/1092.cpp
+++ lc/1092/1092.cpp
@@ -7,61 +7,6 @@
 
 #include <string>
 #include <unordered_map>
-#include <iostream>
-
-class Trie {
-public:
-    template <typename It>
-    void insert(const It& b, const It& e) {
-        if (b != e) {
-            bool terminal { b + 1 == e };
-            if (!m_children.count(*b)) {
-                m_children.insert({*b, std::make_pair(std::make_unique<Trie>(), terminal)});
-            } else {
-                m_children[*b].second = terminal;
-            }
-            m_children[*b].first->insert(b + 1, e);
-        }
-    }
-
-    template <typename It>
-    int match(const It& b, const It& e) {
-        return match(b, e, 0);
-    }
-
-private:
-    template <typename It>
-    int match(const It& b, const It& e, const int prefix) {
-        if (b != e && m_children.count(*b)) {
-            int sum {prefix + 1};
-            int rest {m_children[*b].first->match(b+1, e, sum)};
-            return (m_children[*b].second && !rest) ? sum : rest;
-        }
-        return 0;
-    }
-
-    std::unordered_map<char, std::pair<std::unique_ptr<Trie>,bool>> m_children;
-};
-
-class SuffixTree {
-public:
-    SuffixTree(const std::string& s) {
-        auto b {s.begin()};
-        auto e {s.end()};
-        while (b != e) { m_trie.insert(b++, e); }
-    }
-
-    int match(const std::string::iterator& b, const std::string::iterator& e) {
-        return m_trie.match(b, e);
-    }
-
-    int match(const std::string& s) {
-        return m_trie.match(s.begin(), s.end());
-    }
-
-private:
-    Trie m_trie;
-};
 
 enum class Op {
     match,
@@ -189,10 +134,7 @@ public:
 class Solution {
 public:
     std::string shortestCommonSupersequence(std::string str1, std::string str2) {
-        SuffixTree s1 {str1}, s2 {str2};
-        auto pre1 {s2.match(str1)};
-        auto pre2 {s1.match(str2)};
-        return pre1 > pre2 ? str2 + str1.substr(pre1) : str1 + str2.substr(pre2);
+        return "";
     }
 };
 
