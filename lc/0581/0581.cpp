ffdbf1f283a87115328d633017b9e8efe3d4ed2a
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-02-04 22:02:50 -0800

    Fix remaining boundary issues and add more test cases for accepted solution

--- lc/0581/0581.cpp
+++ lc/0581/0581.cpp
@@ -1,6 +1,6 @@
 // 0581. Shortest Unsorted Continuous Subarray
 // Problem definition: https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
-// ?
+// 2020-02-04
 
 #define CATCH_CONFIG_RUNNER
 #include "../../uva/catch/catch.hpp"
@@ -17,23 +17,21 @@ public:
     int findUnsortedSubarray(std::vector<int>& nums) {
         int min {10001}, max {0};
         auto p1 = std::adjacent_find(nums.begin(), nums.end(), std::greater{});
-        std::cout << "First inflection " << p1 - nums.begin() << " Value " << *p1 << "\n";
         if (p1 != nums.end()) { min = *std::min_element(p1 + 1, nums.end()); }
-        std::cout << "Min " << min << "\n";
         auto p2 = std::adjacent_find(nums.rbegin(), nums.rend(), std::less{});
-        std::cout << "Second inflection " << nums.rend() - p2 << " Value " << *p2 << "\n"; 
         if (p2 != nums.rend()) { max = *std::max_element(p2 + 1, nums.rend()); }
-        std::cout << "Max " << max << "\n";
-        auto start = std::find_if(nums.begin(), p1 + 1, std::bind(std::less{}, _1, min));
-        auto end = std::find_if(nums.rbegin(), p2 + 1, std::bind(std::greater{}, _1, max));
-        int r {nums.rend() - end}, l {start - nums.begin()};
+        auto start = std::find_if(nums.begin(), nums.end(), std::bind(std::less{}, min, _1));
+        auto end = std::find_if(nums.rbegin(), nums.rend(), std::bind(std::greater{}, max, _1));
+        int r {nums.rend() - end - 1}, l {start - nums.begin()};
         return r - l < 0 ? 0 : r - l + 1;
     }
 };
 
 TEST_CASE("LC test cases", "[Shortest Unsorted Continuous Subarray]") {
     std::vector<std::pair<std::vector<int>,int>> input {
-        {{2, 6, 4, 8, 10, 9, 15},5},{{8,9,10,1,2,3,4,5,6,7},10}
+        {{2, 6, 4, 8, 10, 9, 15},5},{{8,9,10,1,2,3,4,5,6,7},10},
+        {{1,2,3,4},0},{{1},0},{{2,2,2,2},0},{{1,1,2,2,3,3,4,4},0},
+        {{6,5,4,3,2,1},6},{{6,5,4,3,2,1,7},6}
     };
 
     SECTION("LC test cases") {
