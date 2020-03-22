f70149f4bf05a309646fb9ea1e7810f137b74e7d
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-03-21 21:58:13 -0700

    Complete initial acceptable solution

--- lc/0075/0075.cpp
+++ lc/0075/0075.cpp
@@ -1,6 +1,6 @@
 // 0075. Sort Colors
 // Problem definition: https://leetcode.com/problems/sort-colors/
-// Accepted ?
+// Accepted 2020-03-21
 
 #define CATCH_CONFIG_RUNNER
 #include "../../uva/catch/catch.hpp"
@@ -10,13 +10,25 @@
 class Solution {
 public:
     void sortColors(std::vector<int>& nums) {
-        
+        int zeros {0}, ones {0}, twos {0};
+        for (const auto n : nums) {
+            if (n == 0) {
+                zeros++;
+            } else if (n == 1) {
+                ones++;
+            } else {
+                twos++;
+            }
+        }
+        std::fill(nums.begin(), nums.begin() + zeros, 0);
+        std::fill(nums.begin() + zeros, nums.begin() + zeros + ones, 1);
+        std::fill(nums.begin() + zeros + ones, nums.end(), 2);
     }
 };
 
 TEST_CASE("LC test cases", "[Sort Colors]") {
     std::vector<std::pair<std::vector<int>,std::vector<int>>> input {
-        {{2,0,2,1,1,0},{0,0,1,1,2,2}}     
+        {{2,0,2,1,1,0},{0,0,1,1,2,2}},{{0},{0}},{{1},{1}},{{2},{2}}
     };
 
     SECTION("LC test cases") {
