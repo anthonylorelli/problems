2562ee9e2a691bc388b701e826838b85156336fc
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-05-05 22:00:06 -0700

    Add basic iterator implementation

--- lc/1305/1305.cpp
+++ lc/1305/1305.cpp
@@ -9,23 +9,75 @@
 #include <vector>
 #include <tuple>
 #include <numeric>
+#include <stack>
 
 /**
  * Definition for a binary tree node.
 */
 struct TreeNode {
     int val;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+class iterator
+{
+public:
+    iterator() = default;
+    iterator(TreeNode* node) {
+        if (node) {
+            m_stack.push(node);
+            while (m_stack.top()->left) {
+                m_stack.push(m_stack.top()->left);
+            }
+        }
+    }
+
+    int operator*() {
+        if (m_stack.empty()) {
+            throw std::exception("Empty stack");
+        }
+        return m_stack.top()->val;
+    }
+
+    iterator& operator++() {
+        if (m_stack.empty()) {
+            throw std::exception("Empty stack");
+        }
+        TreeNode* node {m_stack.top()};
+        m_stack.pop();
+        if (node->right) {
+            m_stack.push(node->right);
+            while (m_stack.top()->left) {
+                m_stack.push(m_stack.top()->left);
+            }
+        }
+        return *this;
+    }
+
+    bool operator==(const iterator& i) {
+        return (m_stack.empty() && i.m_stack.empty()) || (m_stack == i.m_stack);
+    }
+
+    bool operator!=(const iterator& i) {
+        return !(*this == i);
+    }
+
+private:
+    std::stack<TreeNode*> m_stack;
+};
+
 class Solution {
 public:
     std::vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        return {};
+        std::vector<int> result;
+        iterator i1 {root1}, i2 {root2};
+        iterator end;
+        std::merge(i1, end, i2, end, std::back_inserter(result));
+        return result;
     }
 };
 
