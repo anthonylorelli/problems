cd929b3eabfb3cda8c7b4cebe4af15c484a59b6a
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-06-01 21:11:14 -0700

    Set up for 0103

---
+++ lc/0103/0103.cpp
@@ -0,0 +1,60 @@
+// 0103. Binary Tree Zigzag Level Order Traversal
+// Problem definition: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+// Accepted ?
+
+#define CATCH_CONFIG_RUNNER
+#include "../../uva/catch/catch.hpp"
+
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+
+/**
+ * Definition for a binary tree node.
+ */ 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    std::vector<std::vector<int>> zigzagLevelOrder(TreeNode* root) {
+        return {{}};        
+    }
+};
+
+auto speed=[]()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+TEST_CASE("LC test cases", "[Binary Tree Zigzag Level Order Traversal]") {
+    SECTION("Case 1") {
+        auto tree = new TreeNode{3, new TreeNode{9, nullptr, nullptr}, new TreeNode{20, new TreeNode{15}, new TreeNode{7}}};
+        std::vector<std::vector<int>> result = {{15,7},{9,20},{3}};
+        Solution s;
+        REQUIRE(s.levelOrderBottom(tree) == result);
+    }
+    SECTION("Case 2") {
+        auto tree = nullptr;
+        std::vector<std::vector<int>> result = {};
+        Solution s;
+        REQUIRE(s.levelOrderBottom(tree) == result);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
