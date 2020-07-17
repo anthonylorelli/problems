f0082d3ea8ff4015bbc9dcaee7d4efc2a677102a
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-07-16 21:11:03 -0700

    Create scaffold for 0257

---
+++ lc/0257/0257.cpp
@@ -0,0 +1,50 @@
+// 0257. Binary Tree Paths
+// Problem definition: https://leetcode.com/problems/binary-tree-paths/
+// Accepted ?
+
+#define CATCH_CONFIG_RUNNER
+#include "../../uva/catch/catch.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+/**
+ * Definition for a binary tree node.
+ */ 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    std::vector<std::string> binaryTreePaths(TreeNode* root) {
+        return {};
+    }
+};
+
+auto speed=[]() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+// TEST_CASE("LC test cases", "[Complete Binary Tree Inserter]") {
+//     SECTION("Case 1") {
+//         auto tree = new TreeNode{1, new TreeNode{2, new TreeNode(4), new TreeNode(5)}, new TreeNode{3, new TreeNode{6}, nullptr}};
+//         Solution s;
+//         REQUIRE(s.isCousins(tree, 3, 4) == false);
+//     }
+// }
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
