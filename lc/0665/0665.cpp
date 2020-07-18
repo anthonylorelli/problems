9d5b55188ee12a7b9db41c3ba25d99ba505e63df
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-07-17 22:15:18 -0700

    Make first (incorrect) attempt to solve

---
+++ lc/0665/0665.cpp
@@ -0,0 +1,52 @@
+// 0665. Non-decreasing Array
+// Problem definition: https://leetcode.com/problems/non-decreasing-array/
+// Accepted ?
+
+#define CATCH_CONFIG_RUNNER
+#include "../../uva/catch/catch.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    bool checkPossibility(std::vector<int>& nums) {
+        int count {0};
+        auto predicate = std::greater<int>{};
+        auto it = std::adjacent_find(nums.begin(), nums.end(), predicate);
+        while (it != nums.end()) {
+            if (++count > 1) {
+                return false;
+            }
+            it = std::adjacent_find(it + 1, nums.end(), predicate);
+        }
+        return true;
+    }
+};
+
+auto speed=[]() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+TEST_CASE("LC test cases", "[Non-decreasing Array]") {
+    SECTION("Case 1") {
+        std::vector<int> input = {4,2,3};
+        Solution s;
+        REQUIRE(s.checkPossibility(input) == true);
+    }
+    SECTION("Case 2") {
+        std::vector<int> input = {4,2,1};
+        Solution s;
+        REQUIRE(s.checkPossibility(input) == false);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
