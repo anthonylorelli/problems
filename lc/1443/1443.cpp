2e445150369200116b0c8c726fa3d819d68ac5bf
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-08-01 14:37:03 -0700

    Create scaffold for 1443

---
+++ lc/1443/1443.cpp
@@ -0,0 +1,43 @@
+// 1443. Minimum Time to Collect All Apples in a Tree
+// Problem definition: https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/
+// Accepted ?
+
+#define CATCH_CONFIG_RUNNER
+#include "../../uva/catch/catch.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <forward_list>
+#include <limits>
+
+class Solution {
+public:
+    int minTime(int n, std::vector<std::vector<int>>& edges, std::vector<bool>& hasApple) {
+        return 0;
+    }
+};
+
+auto speed=[]() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+TEST_CASE("LC test cases", "[Core]") {
+    SECTION("Case 1") {
+        std::vector<std::vector<int>> edges = {{0,1},{0,2},{1,4},{1,5},{2,3},{2,6}};
+        std::vector<bool> has_apple = {false,false,true,false,true,true,false};
+        int n {4};
+        Solution s;
+        REQUIRE(s.minTime(n, edges, has_apple) == 8);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
