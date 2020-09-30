1d0d1e8952863fdf20a2c7e0a730dd7fdd000b3b
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-09-29 19:56:23 -0700

    Move logic into function as required in problem definition

--- pw/ch1/01.py
+++ pw/ch1/01.py
@@ -1,13 +1,17 @@
 import random
-number = random.randint(1, 100)
 
-while True:
-    guess = int(input("Guess a number: "))
+def guessing_game():
+    number = random.randint(1, 100)
 
-    if guess > number:
-        print("Too high. Guess again.")
-    elif guess < number:
-        print("Too low. Guess again.")
-    else:
-        print("You got it!")
-        break
+    while True:
+        guess = int(input("Guess a number: "))
+
+        if guess > number:
+            print(f"{guess} is too high. Guess again.")
+        elif guess < number:
+            print(f"{guess} is too low. Guess again.")
+        else:
+            print(f"{guess} - you got it!")
+            break
+
+guessing_game()
