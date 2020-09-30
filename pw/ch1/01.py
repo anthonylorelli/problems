b09a00563acf9d94627c5f571d66e8ea633ef883
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-09-29 19:51:16 -0700

    Complete first workout

--- pw/ch1/01.py
+++ pw/ch1/01.py
@@ -1,3 +1,13 @@
 import random
 number = random.randint(1, 100)
-print(number)
+
+while True:
+    guess = int(input("Guess a number: "))
+
+    if guess > number:
+        print("Too high. Guess again.")
+    elif guess < number:
+        print("Too low. Guess again.")
+    else:
+        print("You got it!")
+        break
