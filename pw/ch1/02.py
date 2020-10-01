caee2f45142184797ede03120d1e458c94f89f6a
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-09-30 21:34:46 -0700

    Finish first pass on problem 2

--- pw/ch1/02.py
+++ pw/ch1/02.py
@@ -1,1 +1,13 @@
 # Exercise 2 Summing numbers
+def mysum(*args):
+    sum = 0
+    for n in args:
+        sum += n
+
+    return sum
+
+j = mysum(1, 2)
+k = mysum(2, 3, 4)
+l = mysum(3, 4, 5, 6)
+
+print(f"Sum 1: {j} Sum 2: {k} Sum 3: {l}")
