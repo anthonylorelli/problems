827a4c5cf1254734453933288f24542005853c1f
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-10-07 21:17:28 -0700

    Complete acceptable solution

--- lc/0508/0508.cpp
+++ lc/0508/0508.cpp
@@ -1,6 +1,6 @@
 // 0508. Most Frequent Subtree Sum
 // Problem definition: https://leetcode.com/problems/most-frequent-subtree-sum/
-// Accepted ?
+// Accepted 2020-10-07
 #define CATCH_CONFIG_RUNNER
 #include "../../inc/catch.hpp"
 #include "../../inc/treenode.h"
@@ -9,19 +9,43 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <unordered_map>
 
 class Solution {
 public:
     std::vector<int> findFrequentTreeSum(TreeNode* root) {
-        return {};
+        traverse(root);
+        std::vector<int> result;
+        for (auto [key, value] : m_map) {
+            if (value == m_max) { result.push_back(key); }
+        }
+        return result;
     }
+
+private:
+    int traverse(const TreeNode* node) {
+        if (!node) {
+            return 0;
+        }
+
+        const int sum {node->val + traverse(node->left) + traverse(node->right)};
+        const int count {++m_map[sum]};
+        m_max = std::max(m_max, count);
+
+        return sum;
+    }
+
+    int m_max {0};
+    std::unordered_map<int,int> m_map;
 };
 
 TEST_CASE("LC test cases", "[Core]") {
     Codec c;
     std::vector<std::pair<TreeNode*,std::vector<int>>> input {
         {c.deserialize("[5,2,-3]"),{2,-3,4}},
-        {c.deserialize("[5,2,-5]"),{2}}
+        {c.deserialize("[5,2,-5]"),{2}},
+        {c.deserialize("[]"),{}},
+        {c.deserialize("[19]"),{19}}
     };
 
     SECTION("LC test cases") {
