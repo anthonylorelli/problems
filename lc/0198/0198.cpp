c6bd85517a86e2913e1b56a2d22957fcf727b951
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-10-16 21:42:32 -0700

    Create scaffold for 0198

---
+++ lc/0198/0198.cpp
@@ -0,0 +1,45 @@
+// 0198. House Robber
+// Problem definition: https://leetcode.com/problems/house-robber/
+// Accepted ?
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+#include "../../inc/treenode.h"
+#include "../../inc/serialize.h"
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int rob(std::vector<int>& nums) {
+        return 0;
+    }
+};
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::vector<int>,int> input {
+        {{1,2,3,1},4},
+        {{2,7,9,3,1},12}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) { 
+                Solution s;
+                auto& [testInput, expected] = p;
+                REQUIRE(s.rob(testInput) == expected);
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
