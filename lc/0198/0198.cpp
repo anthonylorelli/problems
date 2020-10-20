49942853ac40d3ba899ea800491b3635da5d1582
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-10-19 21:05:29 -0700

    Set up conditions for loop

--- lc/0198/0198.cpp
+++ lc/0198/0198.cpp
@@ -8,14 +8,22 @@
 
 #include <vector>
 #include <algorithm>
+#include <tuple>
 
 class Solution {
 public:
     int rob(std::vector<int>& nums) {
-        int max {0};
-        for (int i {0}; i < nums.size(); ++i) {
+        if (nums.size() == 0) { return 0; }
+
+        int max {nums[0]};
+        int last {0};
+        std::vector<std::pair<int,int>> sums;
+        sums.push_back({0,nums[0]});
+
+        for (int i {1}; i < nums.size(); ++i) {
 
         }
+
         return max;
     }
 
