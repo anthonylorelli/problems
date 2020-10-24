31b7e2e11e6909c2d73318933dd6100d7e9dc891
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-10-23 20:17:43 -0700

    Complete exercise 7

--- pw/ch2/07.py
+++ pw/ch2/07.py
@@ -1,1 +1,11 @@
 # Exercise 7 Ubbi Dubbi
+def ubbi_dubbi(word):
+    output = []
+    for char in word:
+        if char in 'aeiou':
+            output.append(f"ub{char}")
+        else:
+            output.append(char)
+    return ''.join(output)
+
+print(ubbi_dubbi("elephant"))
