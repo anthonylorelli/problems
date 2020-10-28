d63596b28f82501865e7f7b4f2ecf3f124466ee8
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-10-27 21:19:15 -0700

    Complete acceptable implementation

--- lc/0213/0213.cpp
+++ lc/0213/0213.cpp
@@ -1,6 +1,6 @@
 // 0213. House Robber II
 // Problem definition: https://leetcode.com/problems/house-robber-ii/
-// Accepted ?
+// Accepted 2020-10-27
 #define CATCH_CONFIG_RUNNER
 #include "../../inc/catch.hpp"
 #include "../../inc/treenode.h"
@@ -8,44 +8,45 @@
 
 #include <vector>
 #include <algorithm>
+#include <array>
 
 class Solution {
 public:
     int rob(std::vector<int>& nums) {
-        return 0;
+        auto size {nums.size()};
+        if (size == 0) { return 0; }
+        if (size == 1) { return nums[0]; }
+        if (size == 2) { return std::max(nums[0], nums[1]); }
+
+        int lower = rob(nums, 0, size - 1);
+        int upper = rob(nums, 1, size);
+
+        return std::max(lower, upper);
+    }
+
+private:
+    int rob(std::vector<int>& nums, const int start, const int end) {
+        int maxes[] = {nums[start], nums[start+1]};
+
+        for (int i = start + 2; i < end; ++i) {
+            int j {(i + start) % 2};
+            int next = nums[i] + maxes[j];
+            maxes[(j + 1) % 2] = std::max(maxes[0], maxes[1]);
+            maxes[j] = next;
+        }
+
+        return std::max(maxes[0], maxes[1]);
     }
 };
-
-// class Solution {
-// public:
-//     int rob(std::vector<int>& nums) {
-//         if (nums.size() == 0) { return 0; }
-//         if (nums.size() == 1) { return nums[0]; }
-
-//         int even_max {nums[0]};
-//         int odd_max {nums[1]};
-
-//         for (int i {2}; i < nums.size(); ++i) {
-//             bool even {i % 2 == 0};
-//             int next = nums[i] + (even ? even_max : odd_max);
-//             if (even) {
-//                 odd_max = std::max(odd_max, even_max);
-//                 even_max = next;
-//             } else {
-//                 even_max = std::max(odd_max, even_max);
-//                 odd_max = next;
-//             }
-//         }
-
-//         return std::max(even_max, odd_max);
-//     }
-// };
 
 TEST_CASE("LC test cases", "[Core]") {
     std::vector<std::pair<std::vector<int>,int>> input {
         {{1,2,3,1},4},
         {{2,7,9,3,1},11},
-        {{10,1,2,11},12}
+        {{10,1,2,11},12},
+        {{1},1},
+        {{4,3},4},
+        {{4,3,4},4}
     };
 
     SECTION("LC test cases") {
