9802ed71af6cf7f0fbafeab254ccdcefd0011305
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-10-30 20:55:50 -0700

    Add the I and II versions of the problem as well

---
+++ lc/0112/0112.cpp
@@ -0,0 +1,46 @@
+// 0112. Path Sum
+// Problem definition: https://leetcode.com/problems/path-sum/
+// Accepted ?
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+#include "../../inc/treenode.h"
+#include "../../inc/serialize.h"
+
+#include <vector>
+#include <algorithm>
+#include <array>
+
+class Solution {
+public:
+    int pathSum(TreeNode* root, int sum) {
+        return 0;        
+    }
+};
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::pair<std::pair<std::string,int>,int>> input {
+        {{"[10,5,-3,3,2,null,11,3,-2,null,1]",8}, 7}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) {
+                Codec c; 
+                Solution s;
+                auto& [testInput, expected] = p;
+                REQUIRE(s.pathSum(c.deserialize(testInput.first), testInput.second) == expected);
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
