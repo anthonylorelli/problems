a4f76e6561e332e0040287930224184d4a1ef415
Anthony Lorelli <anthonlo@ntdev.microsoft.com>
2020-11-13 19:39:28 -0800

    Create scaffold for 0155

---
+++ lc/0155/0155.cpp
@@ -0,0 +1,76 @@
+// 0155. Min Stack
+// Problem definition: https://leetcode.com/problems/min-stack/
+// Accepted ?
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+#include "../../inc/listnode.h"
+#include "../../inc/serialize.h"
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        
+    }
+    
+    void pop() {
+        
+    }
+    
+    int top() {
+        
+    }
+    
+    int getMin() {
+        
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::pair<std::tuple<std::vector<int>,int,int>,bool>> input {
+        {{{1,2,3,1},3,0}, true},
+        {{{1,0,1,1},1,2}, true},
+        {{{1,5,9,1,5,9},2,3}, false},
+        {{{},0,0}, false},
+        {{{1},0,0}, false},
+        {{{1,1},1,0}, true},
+        {{{1,2},1,0}, false},
+        {{{8,7,15,1,6,1,9,15},1,3}, true},
+        {{{1,5,9,1,5,9},2,3},false}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) {
+                // Solution s;
+                // auto& [testInput, expected] = p;
+                // auto [input, k, t] = testInput;
+                // REQUIRE(s.containsNearbyAlmostDuplicate(input, k, t) == expected);
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
