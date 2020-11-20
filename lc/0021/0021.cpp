8be7cdb8942a106ab79805cb4886e595c3aee5f0
Anthony Lorelli <anthonlo@ntdev.microsoft.com>
2020-11-19 21:26:37 -0800

    Complete acceptable solution

---
+++ lc/0021/0021.cpp
@@ -0,0 +1,64 @@
+// 0021. Merge Two Sorted Lists
+// Problem definition: https://leetcode.com/problems/merge-two-sorted-lists/
+// Accepted 2020-11-19
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+#include "../../inc/listnode.h"
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode stub;
+        ListNode* ptr {&stub};
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                ptr->next = l1;
+                l1 = l1->next;
+            } else {
+                ptr->next = l2;
+                l2 = l2->next;
+            }
+            ptr = ptr->next;
+        }
+
+        ptr->next = l1 ? l1 : l2;
+        return stub.next;
+    }
+};
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::pair<std::tuple<std::vector<int>,int,int>,bool>> input {
+        {{{1,2,3,1},3,0}, true},
+        {{{1,0,1,1},1,2}, true},
+        {{{1,5,9,1,5,9},2,3}, false},
+        {{{},0,0}, false},
+        {{{1},0,0}, false},
+        {{{1,1},1,0}, true},
+        {{{1,2},1,0}, false},
+        {{{8,7,15,1,6,1,9,15},1,3}, true},
+        {{{1,5,9,1,5,9},2,3},false}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) {
+                // Solution s;
+                // auto& [testInput, expected] = p;
+                // auto [input, k, t] = testInput;
+                // REQUIRE(s.containsNearbyAlmostDuplicate(input, k, t) == expected);
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
