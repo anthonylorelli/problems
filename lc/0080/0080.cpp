76679d26f667d40c1f1364265422dc41c0fa3e5c
Anthony Lorelli <anthonlo@ntdev.microsoft.com>
2020-11-25 20:25:11 -0800

    Create scaffold for 0080

---
+++ lc/0080/0080.cpp
@@ -0,0 +1,51 @@
+// 0080. Remove Duplicates from Sorted Array II
+// Problem definition: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+// Accepted ?
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+
+#include <vector>
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+        return 0;
+    }
+};
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::pair<std::vector<int>,std::pair<std::vector<int>,int>>> input {
+        {{1,2,3},{{1,2,3},3}},
+        {{1,1,2,2,3,3},{{1,1,2,2,3},5}},
+        {{},{{},0}},
+        {{1},{{1},1}},
+        {{1,1,2,3},{{1,1,2,3},4}},
+        {{1,2,3,3,3},{{1,2,3,3},4}},
+        {{1,1,1,1},{{1,1},2}}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) {
+                Solution s;
+                auto& [testInput, expected] = p;
+                auto& [output, count] = expected;
+                REQUIRE(s.removeDuplicates(testInput) == count);
+                for (int i {0}; i < count; ++i) {
+                    REQUIRE(testInput[i] == output[i]);
+                }
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
