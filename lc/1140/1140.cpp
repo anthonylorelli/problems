135983b180964df03589def452be66d10ba9d43f
Anthony Lorelli <anthony.lorelli@outlook.com>
2020-12-24 22:32:32 -0800

    Try alternative implementation

--- lc/1140/1140.cpp
+++ lc/1140/1140.cpp
@@ -12,6 +12,47 @@
 class Solution {
 public:
     int stoneGameII(std::vector<int>& piles) {
+        int n = piles.size();
+        m_sum.resize(n);
+        m_dp.resize(n);
+        for(int i=0;i<n;i++) {
+            m_dp[i].resize(n);
+        }
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                m_dp[i][j] = -1; 
+            }
+        }
+        
+        m_sum[n-1] = piles[n-1];
+        for(int i=n-2; i>=0; i--) {
+            m_sum[i] = m_sum[i+1] + piles[i];
+        }
+            
+        return find_solution(piles, 0, n, 1);
+    }
+
+private:
+    int find_solution(std::vector<int>& arr, int index, int n, int X){
+        if (index >= n) { return 0; }
+        if (m_dp[index][X] != -1) { return m_dp[index][X]; }
+        
+        int min_value = m_sum[index];
+        for (int k=index; k< index + 2 * X && k <n ; k++){
+            min_value = std::min(min_value, find_solution(arr, k+1, n, std::max(k-index+1, X)));
+        }
+        m_dp[index][X] = m_sum[index] - min_value;
+        return m_dp[index][X];
+    }
+
+    std::vector<int> m_sum;
+    std::vector<std::vector<int>> m_dp;
+};
+
+class Solution1 {
+public:
+    int stoneGameII(std::vector<int>& piles) {
         std::vector<int> sums = {0};
         std::partial_sum(piles.begin(), piles.end(), std::back_inserter(sums));
         std::vector<std::vector<int>> dp(sums.size(), std::vector<int>(2 * sums.size() + 1, -1));
