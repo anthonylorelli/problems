58bc967bb1abc331e387c99493f8843fa6d8c988
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-01-03 16:00:37 -0800

    Create scaffold for 0036

---
+++ lc/0036/0036.cpp
@@ -0,0 +1,59 @@
+// 0036. Valid Sudoku
+// Problem definition: https://leetcode.com/problems/valid-sudoku/
+// Accepted ?
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    bool isValidSudoku(std::vector<std::vector<char>>& board) {
+        return false;
+    }
+};
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::pair<std::vector<std::vector<char>>,bool>> input {
+        {{{'5','3','.','.','7','.','.','.','.'},
+          {'6','.','.','1','9','5','.','.','.'},
+          {'.','9','8','.','.','.','.','6','.'},
+          {'8','.','.','.','6','.','.','.','3'},
+          {'4','.','.','8','.','3','.','.','1'},
+          {'7','.','.','.','2','.','.','.','6'},
+          {'.','6','.','.','.','.','2','8','.'},
+          {'.','.','.','4','1','9','.','.','5'},
+          {'.','.','.','.','8','.','.','7','9'}},true},
+        {{{'8','3','.','.','7','.','.','.','.'},
+          {'6','.','.','1','9','5','.','.','.'},
+          {'.','9','8','.','.','.','.','6','.'},
+          {'8','.','.','.','6','.','.','.','3'},
+          {'4','.','.','8','.','3','.','.','1'},
+          {'7','.','.','.','2','.','.','.','6'},
+          {'.','6','.','.','.','.','2','8','.'},
+          {'.','.','.','4','1','9','.','.','5'},
+          {'.','.','.','.','8','.','.','7','9'}},false}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) {
+                Solution s;
+                auto& [testInput, expected] = p;
+                REQUIRE(s.isValidSudoku(testInput) == expected);
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
