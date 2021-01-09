aa476f9da65a3d1511ecfe029c64fad734d606b0
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-01-08 21:00:23 -0800

    Complete acceptable solution for 0153

--- lc/0153/0153.cpp
+++ lc/0153/0153.cpp
@@ -1,6 +1,6 @@
 // 0153. Find Minimum in Rotated Sorted Array
 // Problem definition: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
-// Accepted ?
+// Accepted 2021-01-08
 #define CATCH_CONFIG_RUNNER
 #include "../../inc/catch.hpp"
 
@@ -10,7 +10,21 @@
 class Solution {
 public:
     int findMin(std::vector<int>& nums) {
-        return 0;
+        int left {0};
+        int right = nums.size() - 1;
+        if (nums[left] < nums[right]) {
+            return nums[left];
+        }
+
+        while (right - left > 1) {
+            int mid = (left + right) / 2;
+            if (nums[left] > nums[mid]) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return nums[right];
     }
 };
 
@@ -18,7 +32,10 @@ TEST_CASE("LC test cases", "[Core]") {
     std::vector<std::tuple<std::vector<int>,int>> input {
         {{3,4,5,1,2},1},
         {{4,5,6,7,0,1,2},0},
-        {{11,13,15,17},11}
+        {{11,13,15,17},11},
+        {{-10},-10},
+        {{-10,10},-10},
+        {{10,-10},-10}
     };
 
     SECTION("LC test cases") {
