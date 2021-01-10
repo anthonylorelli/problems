bd6ea54adfce3dbc7798e1f3d2cb701bd6be3407
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-01-09 21:30:05 -0800

    Create scaffold for 0017

--- lc/0017/0017.cpp
+++ lc/0017/0017.cpp
@@ -7,11 +7,19 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 class Solution {
 public:
     std::vector<string> letterCombinations(std::string digits) {
         return {};        
+    }
+
+private:
+    std::unordered_map<char,std::string> {
+        {'2',"abc"}, {'3',"def"}, {'4',"ghi"},
+        {'5',"jkl"}, {'6',"mno"}, {'7',"pqrs"},
+        {'8','tuv'}, {'9',"wxyz"}
     }
 };
 
