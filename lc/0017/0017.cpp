8a8cf9fe583b5ba21e7e6ae14e8ee073c47fc1ee
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-01-09 21:10:10 -0800

    Create scaffold for 0017

---
+++ lc/0017/0017.cpp
@@ -0,0 +1,45 @@
+// 0017. Letter Combinations of a Phone Number
+// Problem definition: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+// Accepted ?
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+
+#include <algorithm>
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    std::vector<string> letterCombinations(std::string digits) {
+        return {};        
+    }
+};
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::tuple<std::string, std::vector<std::string>>> input {
+        {"23",{"ad","ae","af","bd","be","bf","cd","ce","cf"}},
+        {"",{}},
+        {"2",{"a","b","c"}}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) {
+                Solution s;
+                auto& [testInput, target, expected] = p;
+                REQUIRE(s.combinationSum2(testInput, target) == expected);
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
