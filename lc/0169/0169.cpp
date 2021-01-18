5e1605c663ff71ad8e5e215cf29ffd1ff6419162
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-01-17 19:32:24 -0800

    Create scaffold for 0169

---
+++ lc/0169/0169.cpp
@@ -0,0 +1,43 @@
+// 0169. Majority Element
+// Problem definition: https://leetcode.com/problems/majority-element/
+// Accepted ?
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    int majorityElement(std::vector<int>& nums) {
+        return 0;
+    }
+};
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::tuple<std::vector<int>,int>> input {
+        {{3,2,3},3},
+        {{2,2,1,1,1,2,2},2}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) {
+                Solution s;
+                auto& [testInput, expected] = p;
+                REQUIRE(s.majorityElement(testInput) == expected);
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
