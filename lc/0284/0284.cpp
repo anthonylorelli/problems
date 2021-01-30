365145d82c26523d3f285e54585073fba603c0fc
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-01-29 22:22:39 -0800

    Add declaration for Iterator base class

--- lc/0284/0284.cpp
+++ lc/0284/0284.cpp
@@ -10,20 +10,20 @@
 /*
  * Below is the interface for Iterator, which is already defined for you.
  * **DO NOT** modify the interface for Iterator.
- *
- *  class Iterator {
- *		struct Data;
- * 		Data* data;
- *		Iterator(const vector<int>& nums);
- * 		Iterator(const Iterator& iter);
- *
- * 		// Returns the next element in the iteration.
- *		int next();
- *
- *		// Returns true if the iteration has more elements.
- *		bool hasNext() const;
- *	};
  */
+class Iterator {
+public:
+	struct Data;
+	Data* data;
+	Iterator(const std::vector<int>& nums);
+	Iterator(const Iterator& iter);
+
+	// Returns the next element in the iteration.
+	int next();
+
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
 
 class PeekingIterator : public Iterator {
 public:
