465d352aedc3464ae66226c73f61e9561800ac6d
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-03-01 20:45:56 -0800

    Create scaffold for 0151

---
+++ lc/0151/0151.cpp
@@ -0,0 +1,47 @@
+// 0151. Reverse Words in a String
+// Problem definition: https://leetcode.com/problems/reverse-words-in-a-string/
+// Accepted ?
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+
+#include <algorithm>
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    std::string reverseWords(std::string s) {
+        return "";
+    }
+};
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::tuple<std::string,std::string>> input {
+        {"the sky is blue","blue is sky the"},
+        {"  hello world  ","world hello"},
+        {"a good   example","example good a"},
+        {"  Bob    Loves  Alice   ","Alice Loves Bob"},
+        {"Alice does not even like bob","bob like even not does Alice"}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) {
+                Solution s;
+                auto& [testInput, expected] = p;
+                REQUIRE(s.reverseWords(testInput) == expected);
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
