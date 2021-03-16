1d4a9997ce89e812a2e4413c1d67ef33b19a353e
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-03-15 21:59:26 -0700

    Create scaffold for 0004

---
+++ lc/0004/0004.cpp
@@ -0,0 +1,46 @@
+// 0004. Median of Two Sorted Arrays
+// Problem definition: https://leetcode.com/problems/median-of-two-sorted-arrays/
+// Accepted ?
+#define CATCH_CONFIG_RUNNER
+#include "../../inc/catch.hpp"
+
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+        return 0.0;
+    }
+};
+
+TEST_CASE("LC test cases", "[Core]") {
+    std::vector<std::tuple<std::vector<int>,std::vector<int>,double>> input {
+        {{1,3},{2},2.0},
+        {{1,2},{3,4},2.5},
+        {{0,0},{0,0},0.0},
+        {{},{1},1.0},
+        {{2},{},2.0}
+    };
+
+    SECTION("LC test cases") {
+        std::for_each(std::begin(input), std::end(input),
+            [&input](auto& p) {
+                Solution s;
+                auto& [array_a, array_b, expected] = p;
+                REQUIRE(s.findMedianSortedArrays(array_a, array_b) == expected);
+            });
+    }
+}
+
+auto speed=[](){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+int main(int argc, char* argv[]) {
+    std::ios_base::sync_with_stdio(false);
+    return Catch::Session().run(argc, argv);
+}
