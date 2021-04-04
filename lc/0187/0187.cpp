1f0354361aa144f69b54fbcf5861169fa92e47cd
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-04-03 22:26:15 -0700

    Move hashing logic to its own class

--- lc/0187/0187.cpp
+++ lc/0187/0187.cpp
@@ -13,20 +13,24 @@ constexpr uint32_t pow(const uint32_t exp, const uint32_t base, const uint32_t m
     return exp == 0 ? 1 : (pow(exp - 1, base, mod) * base) % mod;
 }
 
-class Solution {
+class RollingHash {
 public:
+
+private:
     constexpr uint32_t hash(const uint32_t h, const char c) const noexcept { 
         return ((h * c_base) + c) % c_mod; 
     }
 
-    std::vector<std::string> findRepeatedDnaSequences(std::string s) {
-        return {};        
-    }
-
-private:
     static constexpr uint32_t c_base {256};
     static constexpr uint32_t c_mod {7778777};
     static constexpr uint32_t c_offset {pow(9, c_base, c_mod)};
+};
+
+class Solution {
+public:
+    std::vector<std::string> findRepeatedDnaSequences(std::string s) {
+        return {};        
+    }
 };
 
 TEST_CASE("LC test cases", "[Core]") {
