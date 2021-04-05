70e20b7c5a411a0b9f417598080aac38b504dd45
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-04-05 12:20:25 -0700

    Complete initial attempt at RollingHash

--- lc/0187/0187.cpp
+++ lc/0187/0187.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <string_view>
 
 constexpr uint32_t pow(const uint32_t exp, const uint32_t base, const uint32_t mod) {
     return exp == 0 ? 1 : (pow(exp - 1, base, mod) * base) % mod;
@@ -15,11 +16,37 @@ constexpr uint32_t pow(const uint32_t exp, const uint32_t base, const uint32_t m
 
 class RollingHash {
 public:
+    RollingHash(std::string_view& s, const uint32_t length) : m_s{s}, m_length{length} { 
+        // We assume length < s.size()
+        while (m_current < length) {
+            m_hash = hash(m_hash, m_s[m_current]);
+            ++m_current;
+        }
+    }
+
+    bool has_next() const noexcept {
+        return m_current < m_s.size();
+    }
+
+    uint32_t next() noexcept {
+        auto cache = m_hash;
+        if (has_next()) {
+            m_hash = ((m_hash + c_mod) - m_s[m_current - m_length]) * c_offset;
+            m_hash = hash(m_hash, m_s[m_current]);
+            ++m_current;
+        }
+        return cache;
+    }
 
 private:
     constexpr uint32_t hash(const uint32_t h, const char c) const noexcept { 
         return ((h * c_base) + c) % c_mod; 
     }
+
+    std::string_view m_s;
+    const uint32_t m_length;
+    uint32_t m_current {0};
+    uint32_t m_hash {0};
 
     static constexpr uint32_t c_base {256};
     static constexpr uint32_t c_mod {7778777};
@@ -33,19 +60,37 @@ public:
     }
 };
 
-TEST_CASE("LC test cases", "[Core]") {
-    std::vector<std::tuple<std::string,std::vector<std::string>>> input {
-        {"AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT",{{"AAAAACCCCC","CCCCCAAAAA"}},
-        {"AAAAAAAAAAAAA",{"AAAAAAAAAA"}}
-    };
+// TEST_CASE("LC test cases", "[Core]") {
+//     std::vector<std::tuple<std::string,std::vector<std::string>>> input {
+//         {"AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT",{"AAAAACCCCC","CCCCCAAAAA"}},
+//         {"AAAAAAAAAAAAA",{"AAAAAAAAAA"}}
+//     };
+
+//     SECTION("LC test cases") {
+//         std::for_each(std::begin(input), std::end(input),
+//             [&input](auto& p) {
+//                 Solution s;
+//                 auto& [testInput, expected] = p;
+//                 REQUIRE(s.findRepeatedDnaSequences(testInput) == expected);
+//             });
+//     }
+// }
+
+TEST_CASE("LC test cases", "[RollingHash class unit tests]") {
+    // std::vector<std::tuple<std::string,std::vector<std::string>>> input {
+    //     {"AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT",{"AAAAACCCCC","CCCCCAAAAA"}},
+    //     {"AAAAAAAAAAAAA",{"AAAAAAAAAA"}}
+    // };
 
     SECTION("LC test cases") {
-        std::for_each(std::begin(input), std::end(input),
-            [&input](auto& p) {
-                Solution s;
-                auto& [testInput, expected] = p;
-                REQUIRE(s.findRepeatedDnaSequences(testInput) == expected);
-            });
+        std::string_view testInput {"AAAAAAAAAAAA"};
+        RollingHash hash(testInput, 10);
+        auto current = hash.next();
+        while (hash.has_next()) {
+            auto next = hash.next();
+            REQUIRE(current == next);
+            current = next;
+        }
     }
 }
 
