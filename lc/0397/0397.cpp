a6c79cac76cb2d6891327bf72ae74a6723f28746
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-04-14 22:59:56 -0700

    Complete acceptable solution

--- lc/0397/0397.cpp
+++ lc/0397/0397.cpp
@@ -1,21 +1,42 @@
 // 0397. Integer Replacement
 // Problem definition: https://leetcode.com/problems/integer-replacement/
-// Accepted ?
+// Accepted 2021-04-14
 #define CATCH_CONFIG_RUNNER
 #include "../../inc/catch.hpp"
 
 #include <algorithm>
+#include <limits>
 
 class Solution {
 public:
     int integerReplacement(int n) {
-        return 0;
+        return calculate(n, 0);
+    }
+
+private:
+    int calculate(int n, int count) {
+        if (n == 1) {
+            return count;
+        }
+        if (n == 3) {
+            return count + 2;
+        }
+        if (n == std::numeric_limits<int>::max()) {
+            return 32;
+        }
+        if ((n&1) == 0) {
+            return calculate(n >> 1, count + 1);
+        }
+        if (int next {n+1}; (next & 3) == 0) {
+            return calculate(next >> 1, count + 2);
+        }
+        return calculate(n - 1, count + 1);
     }
 };
 
 TEST_CASE("LC test cases", "[Core]") {
     std::vector<std::tuple<int,int>> input {
-        {8,3}, {7,4}, {4,2}
+        {8,3}, {7,4}, {4,2}, {std::numeric_limits<int>::max(), 32}
     };
 
     SECTION("LC test cases") {
