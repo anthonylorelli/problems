91910d41415e412f6c0e1bf7bcf1539946440b96
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-06-07 22:11:43 -0700

    Complete acceptable solution for 0191

--- lc/0191.cpp
+++ lc/0191.cpp
@@ -1,6 +1,6 @@
 // 0191. Number of 1 Bits
 // Problem definition: https://leetcode.com/problems/number-of-1-bits/
-// Accepted ?
+// Accepted 2021-06-07
 #define CATCH_CONFIG_RUNNER
 #include "../inc/catch.hpp"
 #include "../inc/treenode.h"
@@ -11,6 +11,37 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        int bits {0};
+        while (n > 0) {
+            bits += count(n & 0xF);
+            n >>= 4;
+        }
+        return bits;
+    }
+
+    constexpr int count(const uint32_t n) {
+        switch (n) {
+        case 15: // 1111
+            return 4;
+        case 14: // 1110
+        case 13: // 1101
+        case 11: // 1011
+        case 7:  // 0111
+            return 3;
+        case 12: // 1100
+        case 10: // 1010
+        case 9:  // 1001
+        case 6:  // 0110
+        case 5:  // 0101
+        case 3:  // 0011
+            return 2;
+        case 8:
+        case 4:
+        case 2:
+        case 1:
+            return 1;
+        }
+
         return 0;
     }
 };
