fc1edfda53024a383e0be2542bc8779adc96b55b
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-07-18 19:08:31 -0700

    Complete acceptable solution

--- lc/0215.cpp
+++ lc/0215.cpp
@@ -1,6 +1,6 @@
 // 0215. Kth Largest Element in an Array
 // Problem definition: https://leetcode.com/problems/kth-largest-element-in-an-array/
-// Accepted ?
+// Accepted 2021-07-18
 #define CATCH_CONFIG_RUNNER
 #include "../inc/catch.hpp"
 
@@ -11,14 +11,17 @@
 class Solution {
 public:
     int findKthLargest(std::vector<int>& nums, int k) {
-        return 0;
+        auto nth = nums.begin() + (nums.size() - k);
+        std::nth_element(nums.begin(), nth, nums.end());
+        return nums[nums.size() - k];
     }
 };
 
 TEST_CASE("LC test cases", "[Core]") {
     std::vector<std::tuple<std::vector<int>,int,int>> input {
         {{3,2,1,5,6,4},2,5},
-        {{3,2,3,1,2,4,5,5,6},4,4}
+        {{3,2,3,1,2,4,5,5,6},4,4},
+        {{1},1,1}
     };
 
     SECTION("LC test cases") {
