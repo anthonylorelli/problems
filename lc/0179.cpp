2db8323803571902e654ae2bde4b5a3e5d35848e
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-07-25 20:47:48 -0700

    Complete acceptable solution

--- lc/0179.cpp
+++ lc/0179.cpp
@@ -1,6 +1,6 @@
 // 0179. Largest Number
 // Problem definition: https://leetcode.com/problems/largest-number/
-// Accepted ?
+// Accepted 2021-07-25
 #define CATCH_CONFIG_RUNNER
 #include "../inc/catch.hpp"
 
@@ -11,13 +11,26 @@
 class Solution {
 public:
     std::string largestNumber(std::vector<int>& nums) {
-        return "";
+        std::vector<std::string> ss;
+        std::transform(nums.begin(), nums.end(), std::back_inserter(ss), [](const auto n) {
+            return std::to_string(n);
+        });
+        std::sort(ss.begin(), ss.end(), [](const auto& a, const auto& b) {
+            return a + b > b + a;
+        });
+        if (ss[0] == "0") { return "0"; }
+        std::string result;
+        for (auto& s : ss) {
+            result += s;
+        }
+        return result;
     }
 };
 
 TEST_CASE("LC test cases", "[Core]") {
     std::vector<std::tuple<std::vector<int>,std::string>> input {
-        {{10,2},"210"},{{3,30,34,5,9},"9534330"},{{1},"1"},{{10},"10"}
+        {{10,2},"210"},{{3,30,34,5,9},"9534330"},{{1},"1"},{{10},"10"},
+        {{0,0,0},"0"}
     };
 
     SECTION("LC test cases") {
