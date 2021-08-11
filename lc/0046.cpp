0907933c8a88bf21893ca91638912e38931923ba
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-08-10 22:30:31 -0700

    Add completed solution for 0046 using prev_/next_permutation

--- lc/0046.cpp
+++ lc/0046.cpp
@@ -1,8 +1,8 @@
 // 0046. Permutations
 // Problem definition: https://leetcode.com/problems/permutations/
-// Accepted 2021-01-12
+// Accepted 2021-08-10
 #define CATCH_CONFIG_RUNNER
-#include "../../inc/catch.hpp"
+#include "../inc/catch.hpp"
 
 #include <algorithm>
 #include <vector>
@@ -10,15 +10,24 @@
 class Solution {
 public:
     std::vector<std::vector<int>> permute(std::vector<int>& nums) {
-        return {};
+        std::vector<int> nums_prev = nums;
+        std::vector<std::vector<int>> result(1, nums);
+        while (std::prev_permutation(nums_prev.begin(), nums_prev.end())) {
+            result.push_back(nums_prev);
+        }
+        while (std::next_permutation(nums.begin(), nums.end())) {
+            result.push_back(nums);
+        }
+        return result;
     }
 };
 
 TEST_CASE("LC test cases", "[Core]") {
     std::vector<std::tuple<std::vector<int>,std::vector<std::vector<int>>>> input {
         {{1,2,3},{{1,2,3},{1,3,2},{2,1,3},{2,3,1},{3,1,2},{3,2,1}}},
-        {{0,1},{{0,1},{1,0}}},
-        {{1},{{1}}}
+        {{0,1},{{0,1},{1,0}}}, {{1},{{1}}},
+        {{3,2,1},{{3,2,1},{3,1,2},{2,3,1},{2,1,3},{1,3,2},{1,2,3}}},
+        {{2,3,1},{{2,3,1},{2,1,3},{3,2,1},{3,1,2},{1,2,3},{1,3,2}}}
     };
 
     SECTION("LC test cases") {
@@ -30,13 +39,6 @@ TEST_CASE("LC test cases", "[Core]") {
             });
     }
 }
-
-auto speed=[](){
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
-}();
 
 int main(int argc, char* argv[]) {
     std::ios_base::sync_with_stdio(false);
