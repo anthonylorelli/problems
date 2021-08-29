94d4ef05356467ea156e2d17966154ea95c83cc2
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-08-28 20:21:33 -0700

    Complete acceptable solution for 0368 using DP

--- lc/0368.cpp
+++ lc/0368.cpp
@@ -1,22 +1,45 @@
 // 0368. Largest Divisible Subset
 // Problem definition: https://leetcode.com/problems/largest-divisible-subset/
-// Accepted ?
+// Accepted 2021-08-28
+// Cf. https://leetcode.com/problems/largest-divisible-subset/discuss/714538/Classical-DP-Similar-to-Longest-Increasing-Subsequence-(LIS)
 #define CATCH_CONFIG_RUNNER
 #include "../inc/catch.hpp"
 
 #include <algorithm>
 #include <vector>
+#include <iostream>
 
 class Solution {
 public:
     std::vector<int> largestDivisibleSubset(std::vector<int>& nums) {
-        return {};
+        std::sort(nums.begin(), nums.end());
+        std::vector<int> dp(nums.size(), 1);
+        int max_len {1};
+        for (int i {0}; i < nums.size(); ++i) {
+            for (int j {0}; j < i; ++j) {
+                if (nums[i] % nums[j] == 0) {
+                    dp[i] = std::max(dp[i], dp[j] + 1);
+                }
+            }
+            max_len = std::max(max_len, dp[i]);
+        }
+
+        std::vector<int> result;
+        int prev {-1};
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (max_len == dp[i] && (prev == -1 || prev % nums[i] == 0)) {
+                result.push_back(nums[i]);
+                max_len--;
+                prev = nums[i];
+            }
+        }
+        return result;
     }
 };
 
 TEST_CASE("LC test cases", "[Core]") {
     std::vector<std::tuple<std::vector<int>,std::vector<int>>> input {
-        {{1,2,3},{1,2}},{{1,2,4,8},{1,2,4,8}}
+        {{1,2,3},{3,1}},{{1,2,4,8},{8,4,2,1}}
     };
 
     SECTION("LC test cases") {
