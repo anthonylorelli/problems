c15d6b25ca7dcdd44bdd76b2b3e2e60603a5ab9f
Anthony Lorelli <anthony.lorelli@outlook.com>
2021-09-30 22:07:37 -0700

    Optimize to constant-time area calc

--- lc/0304.cpp
+++ lc/0304.cpp
@@ -6,32 +6,27 @@
 
 #include <algorithm>
 #include <vector>
-#include <memory>
 #include <numeric>
 
 class NumMatrix {
 public:
     NumMatrix(std::vector<std::vector<int>>& matrix) : 
-        m_sums{std::make_unique<std::vector<std::vector<int>>>(matrix.size(), 
-            std::vector<int>(matrix[0].size()))} {
+        m_sums{std::vector<std::vector<int>>(matrix.size() + 1, 
+            std::vector<int>(matrix[0].size() + 1))} {
         for (size_t i {0}; i < matrix.size(); ++i) {
-            std::partial_sum(matrix[i].begin(), matrix[i].end(), (*m_sums)[i].begin());
+            for (size_t j {0}; j < matrix[0].size(); ++j) {
+                m_sums[i+1][j+1] = m_sums[i+1][j] + m_sums[i][j+1] + matrix[i][j] - m_sums[i][j];
+            }
         }
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int32_t sum {0};
-        for (int32_t i {row1}; i <= row2; ++i) {
-            int32_t row = (*m_sums)[i].back();
-            int32_t left = col1 ? (*m_sums)[i][col1 - 1] : 0;
-            int32_t right = row - (*m_sums)[i][col2];
-            sum += row - left - right;
-        }
-        return sum;
+        return m_sums[row2 + 1][col2 + 1] - m_sums[row1][col2 + 1] - 
+            m_sums[row2 + 1][col1] + m_sums[row1][col1];
     }
 
 private:
-    std::unique_ptr<std::vector<std::vector<int>>> m_sums;
+    std::vector<std::vector<int>> m_sums;
 };
 
 // TEST_CASE("LC test cases", "[Core]") {
