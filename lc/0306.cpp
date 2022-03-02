d5f27eea23cd792025412f6869d05878bfdb36e6
Anthony Lorelli <anthony.lorelli@outlook.com>
2022-03-01 20:59:31 -0800

    Implement string-based carry-style addition

--- lc/0306.cpp
+++ lc/0306.cpp
@@ -14,19 +14,38 @@ public:
         return false;
     }
 
-    bool isAdditiveSequence(size_t a_start, size_t a_size, size_t b_start, size_t b_size, 
-        std::string& num) {
-        
-    }
-
     template <typename It>
     void add_strings(It b1, It e1, It b2, It e2, int32_t carry, std::string& result) {
         int32_t digit {0};
-        
+        if (b1 == e1 && b2 == e2) {
+            if (carry > 0) {
+                result.push_back(carry + '0');
+            }
+            return;
+        }
+
+        int32_t digit_l {0};
+        if (b1 != e1) {
+            digit_l = *b1 - '0';
+            b1++;
+        }
+
+        int32_t digit_r {0};
+        if (b2 != e2) {
+            digit_r = *b2 - '0';
+            b2++;
+        }
+
+        int32_t sum {carry + digit_l + digit_r};
+        add_strings(b1, e1, b2, e2, sum / 10, result);
+
+        result.push_back((sum % 10) + '0');
     }
 
     std::string add(const std::string& s1, const std::string& s2) {
-
+        std::string result;
+        add_strings(s1.rbegin(), s1.rend(), s2.rbegin(), s2.rend(), 0, result);
+        return result;
     }
 };
 
@@ -35,13 +54,23 @@ TEST_CASE("LC test cases", "[Core]") {
         {{"112358", true}, {"199100199", true}}
     };
 
-    SECTION("LC test cases") {
-        std::for_each(std::begin(input), std::end(input),
-            [&input](auto& p) {
-                Solution s;
-                auto& [testInput, expected] = p;
-                REQUIRE(s.isAdditiveNumber(testInput) == expected);
-            });
+    // SECTION("LC test cases") {
+    //     std::for_each(std::begin(input), std::end(input),
+    //         [&input](auto& p) {
+    //             Solution s;
+    //             auto& [testInput, expected] = p;
+    //             REQUIRE(s.isAdditiveNumber(testInput) == expected);
+    //         });
+    // }
+    SECTION("Add strings") {
+        Solution s;
+        std::string s1{"123"};
+        std::string s2{"456"};
+        REQUIRE(std::string{"579"} == s.add(s1, s2));
+
+        s1 = "1";
+        s2 = "999";
+        REQUIRE(std::string{"1000"} == s.add(s1, s2));
     }
 }
 
