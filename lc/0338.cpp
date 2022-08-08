2789dfab2ddd900287dfcf81f3834048412a2526
Anthony Lorelli <anthony.lorelli@outlook.coms>
2022-08-07 21:43:56 -0700

    Calculate bit counts at compile time using constexpr

--- lc/0338.cpp
+++ lc/0338.cpp
@@ -5,9 +5,43 @@
 #include "../inc/catch.hpp"
 
 #include <algorithm>
+#include <array>
+#include <numeric>
 #include <vector>
 
+constexpr int32_t c_max_size {50001};
+
+class BitCount {
+public:
+    constexpr BitCount() : m_arr{} {
+        std::iota(m_arr.begin(), m_arr.end(), 0);
+        int32_t largest_2 {2};
+        auto start {m_arr.begin() + 2};
+        std::transform(start, m_arr.end(), start, [&](const auto n) {
+            if ((n / largest_2) > 1) { largest_2 <<= 1; }
+            const int32_t rem {n - largest_2};
+            return m_arr[rem] + 1;
+        });
+    }
+
+    const auto begin() const { return m_arr.begin(); }
+    const auto end() const { return m_arr.end(); }
+
+private:
+    std::array<int32_t,c_max_size> m_arr;
+};
+
 class Solution {
+public:
+    std::vector<int> countBits(int n) {
+        constexpr BitCount count;
+        std::vector<int> result;
+        std::copy(count.begin(), count.begin() + n + 1, std::back_inserter(result));
+        return result;
+    }
+};
+
+class SolutionIter {
 public:
     std::vector<int> countBits(int n) {
         if (n == 0) { return {0}; }
